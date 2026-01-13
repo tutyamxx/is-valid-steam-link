60cdec9ef900c271351d5ebb9f3be7adaf55bdf0
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2026-01-13 22:09:39 +0000

    UPDATE: Updated workflow for new npm tokens and fixed vulnerabilities

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  *  is-valid-steam-link - Checks if the url provided is a valid λ Steam url/link
- *  @version: v1.0.3
+ *  @version: v1.0.4
  *  @link: https://github.com/tutyamxx/is-valid-steam-link
  *  @license: MIT
  **/
