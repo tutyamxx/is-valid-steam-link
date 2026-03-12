2c27d4c6899be9bfb0f07bb3ffc38b92093eb656
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2026-03-12 18:24:14 +0000

    UPDATE: Workflow update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  *  is-valid-steam-link - Checks if the url provided is a valid λ Steam url/link
- *  @version: v1.0.8
+ *  @version: v1.0.9
  *  @link: https://github.com/tutyamxx/is-valid-steam-link
  *  @license: MIT
  **/
